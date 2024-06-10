cd2945c68ec4418d2092046ffa544e954e163ba4
Krishna <krshnwrk01@gmail.com>
2024-06-10 22:30:25 +0530

    Create a Doubly LinkedList

---
+++ LinkedList/doublyLinkedList.js
@@ -0,0 +1,14 @@
+class Node{
+    constructor(data, next = null, prev = null){
+        this.data = data
+        this.next = next
+        this.prev = prev
+    }
+}
+
+class DoublyLinkedlist{
+    constructor(){
+        this.head = null
+        this.tail = null
+    }
+}
