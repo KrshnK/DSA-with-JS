a4974d4fc52e1a935343afc1a5aa1c30dc701795
Krishna <krshnwrk01@gmail.com>
2024-06-11 15:26:53 +0530

    Add insertAtEnd method to DoublyLinkedList

--- LinkedList/doublyLinkedList.js
+++ LinkedList/doublyLinkedList.js
@@ -16,14 +16,32 @@ class DoublyLinkedlist{
 // insert at beginning
 
 DoublyLinkedlist.prototype.insertAtBeginning = function(data){
-    const newNode = new Node(data,this.head,null)
+    const newNode = new Node(data, this.head, null)
 
     if(this.head !== null){
         this.head.prev = newNode
     }
 
     this.head = newNode
+
     if(this.tail === null){
         this.tail = newNode
     }
+}
+
+// insert at end
+
+DoublyLinkedlist.prototype.insertAtEnd = function(data){
+    const newNode = new Node(data, null, this.tail)
+
+    if (this.tail !== null) {
+        this.tail.next = newNode
+    }
+
+    this.tail = newNode
+
+    if (this.head === null) {
+        this.head = newNode
+    }
+
 }
