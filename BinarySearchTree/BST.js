aed07e141f1eb6f28f32cc63a0b46ec09fe68bbf
Krishna <krshnwrk01@gmail.com>
2024-06-13 14:48:09 +0530

    Add insert method in BinarySearchTree class

--- BinarySearchTree/BST.js
+++ BinarySearchTree/BST.js
@@ -10,4 +10,31 @@ class BinarySearchTree{
     constructor(){
         this.root = null
     }
+
+    // insert
+    insert(key){
+        const newNode = new BSTNode()
+        if(this.rool === null){
+            this.root = newNode
+        } else {
+            this.insertNode(this.root,newNode)
+        }
+    }
+
+    insertNode(node, newNode){ // node -> root node
+        if(newNode.key < node.key){
+            if(node.left === null){
+                node.left = newNode
+            } else {
+                this.insertNode(node.left,newNode)
+            }
+        } else {
+            if(node.right === null){
+                node.right = newNode
+            } else {
+                this.insertNode(node.right, newNode)
+            }
+        }
+    }
+    
 }
