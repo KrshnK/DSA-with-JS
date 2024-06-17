c1efa8d3cb21ffd49744d7d52d1d735730defdd5
Krishna <krshnwrk01@gmail.com>
2024-06-17 19:15:37 +0530

    Add BFS

---
+++ BinarySearchTree/BFS.js
@@ -0,0 +1,34 @@
+// class BTNode {
+//     constructor(key) {
+//         this.key = key
+//         this.left = null
+//         this.right = null
+//     }
+// }
+
+// class BinaryTree {
+//     constructor() {
+//         this.root = null
+//     }
+
+const BreadthFirstTraversal = (root) => {
+    const values = []
+    const queue = [ root ]
+
+    if(root === null){
+        return values
+    }
+
+    while(queue.length > 0){
+        const node = queue.shift()
+        values.push(node.key)
+
+        if(node.left !== null){
+            queue.push(node.left)
+        }
+        if(node.right !== null){
+            queue.push(node.right)
+        }
+    }
+    return values
+}
