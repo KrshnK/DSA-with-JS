05461c3b0e01549215e737b3646169484623cb93
Krishna <krshnwrk01@gmail.com>
2024-06-24 00:19:52 +0530

    Add Comments

--- Queue/index.js
+++ Queue/index.js
@@ -1,3 +1,15 @@
+/* Queues are a type of data structure where items are stored in a First In First Out (FIFO) order.
+ In a queue, items are added to the back and removed from the front.
+ The Queue class has the following methods:
+ enqueue(data): Adds an item to the queue.
+ -dequeue(): Removes the first item from the queue.
+ -isEmpty(): Checks if the queue is empty.
+ -peek(): Returns the first item in the queue.
+ -size(): Returns the number of items in the queue.
+ -clear(): Removes all items from the queue.
+ -rintQueue(): Returns a string representation of the queue.
+
+ Queue class with enqueue, dequeue, isEmpty, peek, size, clear, printQueue methods */
 class Queue{
     constructor(){
         this.queue = []
--- Stack/index.js
+++ Stack/index.js
@@ -1,3 +1,22 @@
+/* Stack Data Structure in JavaScript
+  Stack is a linear data structure which follows a particular order in which the operations are performed.
+  The order may be LIFO(Last In First Out) or FILO(First In Last Out).
+  Mainly the following three basic operations are performed in the stack:
+      - Push: Adds an item in the stack. If the stack is full, then it is said to be an Overflow condition.
+      - Pop: Removes an item from the stack. The items are popped in the reversed order in which they are pushed. If the stack is empty, then it is said to be an Underflow condition.
+      - Peek or Top: Returns the top element of the stack.
+  The Stack class has the following methods:
+      - push(data): Adds an item to the stack.
+      - pop(): Removes the last item from the stack.
+      - peek(): Returns the last item in the stack.
+      - isEmpty(): Checks if the stack is empty.
+      - size(): Returns the number of items in the stack.
+      - clear(): Removes all items from the stack.
+      - contains(element): Checks if the stack contains the specified element.
+      - reverse(): Reverses the order of the stack items.
+      - printStack(): Returns a string representation of the stack.
+
+ Stack class with push, pop, peek, isEmpty, size, clear, contains, reverse, printStack methods  */
 class Stack{
     constructor(){
         this.stack= []
